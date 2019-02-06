dd1c97de7670e214bb574e64e0a3bedbed65ea0b
micwimmer <michael.wimmer@salzburgresearch.at>
2019-02-06 14:18:37 +0100

    removed unused import

--- api/src/main/java/at/srfg/graphium/api/controller/AbstractGraphApiController.java
+++ api/src/main/java/at/srfg/graphium/api/controller/AbstractGraphApiController.java
@@ -25,7 +25,6 @@ import java.util.zip.GZIPOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
