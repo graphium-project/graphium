ea1179b2c1510518a05d3ef862d2c1704660b91e
SGroe <simon.groechenig@salzburgresearch.at>
2021-02-05 10:53:01 +0100

    Add hd capability

---
+++ graphium-hd/src/main/java/at/srfg/graphium/core/capabilities/impl/GraphiumHdCapabilityImpl.java
@@ -0,0 +1,16 @@
+package at.srfg.graphium.core.capabilities.impl;
+
+import at.srfg.graphium.core.capabilities.ICapability;
+
+/**
+ * @author sgroeche
+ *
+ */
+public class GraphiumHdCapabilityImpl implements ICapability {
+
+	@Override
+	public String getCapability() {
+		return "hd";
+	}
+	
+}
