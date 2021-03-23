6766c8998cbf15a42dcd7d6eacff87b6a29b117e
Stefan Eisl <seisl.mmt-m2020@fh-salzburg.ac.at>
2021-03-23 09:28:06 +0100

    changed the postgis image to a working one and afixed the db migration script

--- postgis/db/composite/create_graphium_database.sql
+++ postgis/db/composite/create_graphium_database.sql
@@ -334,6 +334,26 @@ ALTER TABLE graphs.graph_import_states
 
 select graphs.db_schema_changed(7, '07_from_v6_to_v7_correct_subscription_fk_constraints.sql');
 
+CREATE TABLE graphs.hdwaysegments
+(
+  left_border_geometry geometry(LINESTRING, 4326),
+  left_border_startnode_id bigint NOT NULL,
+  left_border_endnode_id bigint NOT NULL,
+  right_border_geometry geometry(LINESTRING, 4326),
+  right_border_startnode_id bigint NOT NULL,
+  right_border_endnode_id bigint NOT NULL,
+  CONSTRAINT graphs_hdwaysegments_pk PRIMARY KEY (id)
+) INHERITS (graphs.waysegments)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE graphs.hdwaysegments OWNER TO graphium;
+
+ALTER TABLE graphs.waysegment_connections ADD COLUMN tags hstore;
+
+select graphs.db_schema_changed(9, '09_from_v8_to_v9_update_to_hdwaysegments.sql');
+
+
 CREATE FUNCTION f_current_graphversion_immutable(in_graph text, in_version text)
   RETURNS integer AS
 $func$
