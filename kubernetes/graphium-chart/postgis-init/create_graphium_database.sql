cea7bd66a037889fc8be37a0dc8fa46486e08c99
Stefan Eisl <seisl.mmt-m2020@fh-salzburg.ac.at>
2021-09-12 22:44:49 +0200

    adopted helm values and bumped version of the chart

--- kubernetes/graphium-chart/postgis-init/create_graphium_database.sql
+++ kubernetes/graphium-chart/postgis-init/create_graphium_database.sql
@@ -349,3 +349,26 @@ $func$
 $func$  LANGUAGE sql IMMUTABLE;
 ALTER FUNCTION f_current_graphversion_immutable(text, text)
   OWNER TO graphium;
+
+INSERT INTO graphs.accesses ("type", id) VALUES ('NONE', -1);
+
+SELECT graphs.db_schema_changed(8, '08_from_v7_to_v8_add_access_type_none.sql');
+
+CREATE TABLE graphs.hdwaysegments
+(
+  left_border_geometry geometry(LINESTRING, 4326),
+  left_border_startnode_id bigint NOT NULL,
+  left_border_endnode_id bigint NOT NULL,
+  right_border_geometry geometry(LINESTRING, 4326),
+  right_border_startnode_id bigint NOT NULL,
+  right_border_endnode_id bigint NOT NULL,
+  CONSTRAINT graphs_hdwaysegments_pk PRIMARY KEY (id)
+) INHERITS (graphs.waysegments)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE graphs.hdwaysegments OWNER TO graphium;
+
+ALTER TABLE graphs.waysegment_connections ADD COLUMN tags hstore;
+
+select graphs.db_schema_changed(9, '09_from_v8_to_v9_update_to_hdwaysegments.sql');
